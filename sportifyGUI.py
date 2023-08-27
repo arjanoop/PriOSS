080991ea17a5eab68cf8718472848bab6af7efef
Anoop RJ <arjanoop@gmail.com>
2023-08-27 06:13:23 +0200

    updated variable names

--- sportifyGUI.py
+++ sportifyGUI.py
@@ -3,6 +3,6 @@ import json
 with open("sampledata/Userdata.json", "r", encoding="utf-8") as json_file:
     userData = json.load(json_file)
 with open("sampledata/StreamingHistory.json", "r", encoding="utf-8") as json_file:
-    userData = json.load(json_file)
+    streamingHistory = json.load(json_file)
 with open("sampledata/Inferences.json", "r", encoding="utf-8") as json_file:
-    userData = json.load(json_file)
+    inferences = json.load(json_file)
